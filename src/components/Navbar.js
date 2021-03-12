0b3d6a75f7a2507d41dad6e1546ef5081969924c
Robert Koepp <rkoepp2@gmail.com>
2021-03-11 18:07:55 -0800

    created shell and started routing

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+
+class Navbar extends React.Component{
+    render(){
+        return (
+            <>   
+            <h1>Home</h1>
+            <h2>Contact</h2>
+            <h3>Projects</h3>
+        </>
+        )
+    }
+}
+export default Navbar
---
+++ src/pages/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+
+const Project = () => {
+    return(
+        <>   
+            <h1>
+                This is the project page
+            </h1>
+        </>
+    )
+}
+export default Project
